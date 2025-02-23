b347217672dcae71d47c8fd34e9fea968dd55831
pryrotech <colbypry@gmail.com>
2025-02-23 15:09:41 -0330

    Intial commit of PIM-IT

---
+++ PIM-IT_CLI.ps1
@@ -0,0 +1,44 @@
+###########################################
+#         PIM-IT PowerShell Tool          #
+#        By Colby Pryor (PryroTech)       #
+###########################################
+
+# Import and package
+# Import-MgGraph
+
+# Connect to Microsoft Graph with the required scopes
+Connect-MgGraph -Scopes "User.Read", "RoleManagement.ReadWrite.Directory" -NoWelcome
+
+# Set variable for program and menu
+$programStatus = 1
+
+# Retrieve the user ID for a specific UPN
+$user = Get-MgUser -UserId 'cpryor@pryrotechsandbox.onmicrosoft.com'
+$userId = $user.Id
+
+# Get the eligible roles for the user based on the user ID
+$eligibleRoles = Get-MgRoleManagementDirectoryRoleEligibilityScheduleInstance -Filter "principalId eq '$userId'"
+
+
+while($programStatus -eq 1){
+    # Output welcome message and eligible roles
+Write-Output "******************************"
+Write-Output "* Welcome to PIM-IT CLI Tool *"
+Write-Output "******************************"
+
+Write-Output "`nHello $($user.UserPrincipalName), please select one of your eligible roles beclow:"
+if ($eligibleRoles -eq $null -or $eligibleRoles.Count -eq 0) {
+    $eligibleRoles = Read-Host "No roles found. Scan for new roles? (Y/N)"
+
+    if($eligibleRoles -eq "y"){
+        Write-Output "Scanning..."
+        $eligibleRoles = Get-MgRoleManagementDirectoryRoleEligibilityScheduleInstance -Filter "principalId eq '$userId'"
+    }
+
+    if($eligibleRoles -eq "n"){
+        $programStatus = 0
+    }
+}
+Write-Output $eligibleRoles
+}
+Write-Output "Have a good day! Exiting..."
