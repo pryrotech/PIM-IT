b045071f636dd2df2f8bb17c0a2bc5658edc6887
pryrotech <colbypry@gmail.com>
2025-04-05 13:32:44 -0230

    Made some changes and started using MSGraph versus AzureAD cmdlet

--- PIM-IT_CLI.ps1
+++ PIM-IT_CLI.ps1
@@ -3,46 +3,14 @@
 #        By Colby Pryor (PryroTech)       #
 ###########################################
 
-# Install packages if required
-Install-Module Microsoft.Graph
+#Imports
+Import-Module Microsoft.Graph.Beta.Identity.Governance
 
-# Connect to Microsoft Graph with the required scopes
-Connect-MgGraph -Scopes "User.Read", "RoleManagement.ReadWrite.Directory" -NoWelcome
+#Connect to Mg-Graph
+Connect-MgGraph -Scopes "PrivilegedAccess.ReadWrite.AzureADGroup"
+$user = "//"
 
-# Set variable for program and menu
-$programStatus = 1
+#Get all eligible PIM roles
+$roles = Get-MgBetaRoleManagementDirectoryRoleAssignmentSchedule -All  -Filter "PrincipalId eq '$($user.Id)'"
 
-# Retrieve the user ID for a specific UPN
-$user = Get-MgUser -UserId 'cpryor@pryrotechsandbox.onmicrosoft.com'
-$userId = $user.Id
-
-# Get the eligible roles for the user based on the user ID
-$eligibleRoles = Get-MgRoleManagementDirectoryRoleEligibilityScheduleInstance -Filter "principalId eq '$userId'"
-
-
-while($programStatus -eq 1){
-    # Output welcome message and eligible roles
-Write-Output "******************************"
-Write-Output "* Welcome to PIM-IT CLI Tool *"
-Write-Output "******************************"
-
-if ($eligibleRoles -eq $null -or $eligibleRoles.Count -eq 0) {
-    $eligibleRoles = Read-Host "No roles found. Scan for new roles? (Y/N)"
-
-    if($eligibleRoles -eq "y"){
-        Write-Output "Scanning..."
-        $eligibleRoles = Get-MgRoleManagementDirectoryRoleEligibilityScheduleInstance -Filter "principalId eq '$userId'"
-    }
-
-    if($eligibleRoles -eq "n"){
-        $programStatus = 0
-    }
-}
-
-if ($eligibleRoles -ne $null -or $eligibleRoles.Count -gt 0) {
-    Write-Output "`nHello $($user.UserPrincipalName), please select one of your eligible roles below:"
-    Write-Output $eligibleRoles
-}
-
-}
-Write-Output "Have a good day! Exiting."
+Write-Output $roles.RoleDefinition.DisplayName
